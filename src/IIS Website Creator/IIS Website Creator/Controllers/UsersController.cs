e7af2625c9b6326d0fea63094566a9dc4f015b70
d.neykov <dimitarneikov@gmail.com>
2023-06-16 21:36:03 +0300

    Add Users Contoller

---
+++ src/IIS Website Creator/IIS Website Creator/Controllers/UsersController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.DirectoryServices.AccountManagement;
+
+namespace IIS_Website_Creator.Controllers
+{
+    public class UsersController : BaseController
+    {
+        [HttpPost]
+        public IActionResult Create(string username, string password)
+        {
+            try
+            {
+                using (PrincipalContext context = new PrincipalContext(ContextType.Machine))
+                {
+                    UserPrincipal user = new UserPrincipal(context);
+                    user.SamAccountName = username;
+                    user.SetPassword(password);
+                    user.Enabled = true;
+                    user.Save();
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+
+            return this.StatusCode(201);
+        }
+    }
+}
